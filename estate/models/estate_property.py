3a3329ba058fb07975f82a0390e8f8a3444c2933
Stéphane Delbeke <stephane.delbeke@enabel.be>
2025-09-16 14:12:09 +0200

    quick fix

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -22,7 +22,7 @@ class EstateProperty(models.Model):
                              copy=False,
                              required=True)
     postcode = fields.Char()
-    facades = fields.Interger()
+    facades = fields.Integer()
     garage = fields.Boolean()
     garden_area = fields.Integer()
     living_area = fields.Integer()
