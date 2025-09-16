f0ae9491934ab715454a41a468ee8fb9977935d3
Stéphane Delbeke <stephane.delbeke@enabel.be>
2025-09-16 13:29:35 +0200

    add custom list view

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,7 +6,7 @@
     "data": [
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
-        'views/estate_menu.xml'
+        'views/estate_menu.xml',
     ],
     "installable": True,
     'license': 'LGPL-3',
