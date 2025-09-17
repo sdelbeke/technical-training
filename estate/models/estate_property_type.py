d54c8021046b97086c84364e6a43d02bd6aebcc6
Stéphane Delbeke <stephane.delbeke@enabel.be>
2025-09-17 20:59:03 +0200

    type manual ordering

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -4,8 +4,9 @@ from odoo import fields, models
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
     _description = "Estate Property Type"
-    _order = "name desc"
+    _order = "sequence, name desc"
 
 
     name = fields.Char(required=True)
     property_ids = fields.One2many("estate.property","property_type_id")
+    sequence = fields.Integer('Sequence', default = 1, help="Use to order types")
